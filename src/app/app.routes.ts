e4b3df72838894f2df808f66ba5bb36816591733
Jesus Sierra <sierraja06@gmail.com>
2024-07-16 14:23:46 -0500

    Añadiendo rutas al Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CharactersComponent } from './components/characters/characters.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:CharactersComponent
+    }
+];
