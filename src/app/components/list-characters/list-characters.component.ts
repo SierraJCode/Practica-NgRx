7dccfa13103578c07d5adf0bb9ec2028600e1d07
Jesus Sierra <sierraja06@gmail.com>
2024-07-17 18:26:38 -0500

    Animaciones de los divs de personajes creada

--- src/app/components/list-characters/list-characters.component.ts
+++ src/app/components/list-characters/list-characters.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
 import { CharactersService } from '../../services/characters.service';
 import { CommonModule } from '@angular/common';
+import { LoadingComponent } from '../loading/loading.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-characters',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingComponent, FormsModule],
   templateUrl: './list-characters.component.html',
   styleUrl: './list-characters.component.css'
 })
 export class ListCharactersComponent {
 
   characters: any[] = [];
+  allCharacters: any[] = []
   numPage: number = 1;
   nextDisable: boolean = false;
   prevDisable: boolean = false;
   visibility: string = 'visibility';
   star: string = 'star'
+  charactersCharged: boolean = false;
 
   constructor(private charactersService: CharactersService){}
 
@@ -24,9 +28,20 @@ export class ListCharactersComponent {
     this.getCharacters();
   }
 
-  getCharacters(){
+  searchCharacter(){
+    
+  }
+
+  async getCharacters(){
+    this.charactersCharged = false;
     return this.charactersService.getCharacters(this.numPage).subscribe(
-      res => this.characters = res.results
+      res => {
+        this.characters = res.results
+        setTimeout(() =>{
+          this.charactersCharged = true
+        },1)
+        
+      }
     )
   }
 
