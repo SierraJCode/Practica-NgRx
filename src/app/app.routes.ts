2f83bcc9fcfe33193a9f41b2c708cf4ba24193d8
Jesus Sierra <sierraja06@gmail.com>
2024-07-16 22:38:48 -0500

    Cambio de ruta principal al nuevo componnete list-characters

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CharactersComponent } from './components/characters/characters.component';
+import { ListCharactersComponent } from './components/list-characters/list-characters.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component:CharactersComponent
+        component:ListCharactersComponent
     }
 ];
