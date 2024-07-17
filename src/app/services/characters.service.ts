db2f4caff9d6abba754c8b0a0cb391cfc803b5e3
Jesus Sierra <sierraja06@gmail.com>
2024-07-17 00:26:56 -0500

    Pasando parametro de pagina

--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -11,7 +11,7 @@ export class CharactersService {
 
   constructor(private http: HttpClient) { }
 
-  getCharacters():Observable<any>{
-    return this.http.get<any>(`${this.apiUrl}`)
+  getCharacters(numPage: number):Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/character/?page=${numPage}`)
   }
 }
