80828e5f7454dc05965eaa70fc58dc77eb0affbb
Jesus Sierra <sierraja06@gmail.com>
2024-07-20 14:56:54 -0500

    Se creó componente Favoritos

---
+++ src/app/components/favorites/favorites.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites',
+  standalone: true,
+  imports: [],
+  templateUrl: './favorites.component.html',
+  styleUrl: './favorites.component.css'
+})
+export class FavoritesComponent {
+  listFav: number[] = []
+  
+}
