248d05aa11264fca30dcc3c769b5ce109727274e
Jesus Sierra <sierraja06@gmail.com>
2024-07-22 02:22:32 -0500

    boton favorito añadido

--- src/app/components/list-characters/list-characters.component.ts
+++ src/app/components/list-characters/list-characters.component.ts
@@ -5,102 +5,93 @@ import { LoadingComponent } from '../loading/loading.component';
 import { FormsModule } from '@angular/forms';
 import { FavoritesComponent } from '../favorites/favorites.component';
 import { Character } from '../interfaces/character.interface';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { addFav, removeFav } from '../../state/favorite/favorite.actions';
 
 @Component({
   selector: 'app-list-characters',
   standalone: true,
   imports: [CommonModule, LoadingComponent, FormsModule, FavoritesComponent],
   templateUrl: './list-characters.component.html',
-  styleUrl: './list-characters.component.css'
+  styleUrls: ['./list-characters.component.css']
 })
 export class ListCharactersComponent {
-
   characters: Character[] = [];
   numPage: number = 1;
-  pages: number = 0
+  pages: number = 0;
   nextDisable: boolean = false;
   prevDisable: boolean = true;
-  star: string = 'star'
+  star: string = 'star';
   charactersCharged: boolean = false;
-  input: string = ''
+  input: string = '';
+  characters$: Observable<Character[]>;
 
-  constructor(private charactersService: CharactersService){}
+  constructor(
+    private charactersService: CharactersService, 
+    private store: Store<{ characters: Character[] }>){
+      this.characters$ = store.select('characters')
+    }
 
-  ngOnInit(){
+  ngOnInit() {
     this.getCharacters();
   }
 
-  async restartPage(){
-    this.numPage = 1
-    // if(this.numPage == this.pages){
-    //   this.nextDisable = true;
-    //   this.prevDisable = true;
-    // }else{
-    //   this.nextDisable = false;
-    // }
+  restartPage() {
+    this.numPage = 1;
+    this.getCharacters();
   }
 
-  async getCharacters(){
-    if (this.input === ''){
-      this.charactersCharged = false;
-      return this.charactersService.getCharacters(this.numPage).subscribe(
-        res => {
-          this.characters = res.results.map((character: Character) => ({
-            ...character,
-            showID: true,
-            favorite: false,
-          }));
-          this.pages = res.info.pages;
-          setTimeout(() =>{
-            this.charactersCharged = true
-          },1)
-        }
-      )
-    } else {
-      this.charactersCharged = false;
-      return this.charactersService.getCharacter(this.numPage, this.input).subscribe(
-        res => {
-          this.pages = res.info.pages;
-          this.characters = res.results
-          setTimeout(() =>{
-            this.charactersCharged = true
-          },1)
-        }
-      )
-    }
+  getCharacters() {
+    this.charactersCharged = false;
+    const request = this.input === '' 
+      ? this.charactersService.getCharacters(this.numPage)
+      : this.charactersService.getCharacter(this.numPage, this.input);
+
+    request.subscribe(res => {
+      this.characters = res.results.map((character: Character) => ({
+        ...character,
+        showID: true,
+        favorite: false,
+      }));
+      this.pages = res.info.pages;
+      setTimeout(() => {
+        this.charactersCharged = true;
+      }, 1);
+    });
   }
 
-  showID(character: Character){
+  showID(character: Character) {
     character.showID = !character.showID;
   }
 
-  addFav(id: number){
-    
+  addFav(character: Character) {
+    character.favorite = true
+    this.store.dispatch(addFav({ character }));
+    alert('Add')
+  }
+  
+  removeFav(character: Character, id: number) {
+    character.favorite = false
+    this.store.dispatch(removeFav({ id }));
+    alert('Remove')
   }
 
-  btnNext(){
-    if(this.numPage < this.pages-1){
-      this.numPage += 1;
-      this.prevDisable = false;
-      this.getCharacters()
-    }else if(this.numPage === this.pages-1){
-      this.numPage += 1
-      this.nextDisable = true
-      this.getCharacters()
-    }
-
-  }
-
-  btnPrev(){
-    if(this.numPage > 2){
-      this.numPage -= 1;
-      this.nextDisable = false
-      this.getCharacters()
-    }else if(this.numPage === 2){
-      this.numPage -= 1
-      this.prevDisable = true
-      this.getCharacters()
+  btnNext() {
+    if (this.numPage < this.pages) {
+      this.numPage++;
+      this.prevDisable = this.numPage === 1;
+      this.nextDisable = this.numPage === this.pages;
+      this.getCharacters();
     }
   }
 
+  btnPrev() {
+    if (this.numPage > 1) {
+      this.numPage--;
+      this.prevDisable = this.numPage === 1;
+      this.nextDisable = this.numPage === this.pages;
+      this.getCharacters();
+    }
+  }
 }
