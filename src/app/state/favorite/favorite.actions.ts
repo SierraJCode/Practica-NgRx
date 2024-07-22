fb5a6eb4e9d7ca1d57503bcbfb5c8e50e8f5400e
Jesus Sierra <sierraja06@gmail.com>
2024-07-22 00:56:41 -0500

    Modificanco antion para que acepte el objeto correctamente

--- src/app/state/favorite/favorite.actions.ts
+++ src/app/state/favorite/favorite.actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from "@ngrx/store";
+import { Character } from "../../components/interfaces/character.interface";
 
-export const addFav = createAction('[listCharacters] Add to favorite')
-export const removeFav = createAction('[listCharacters] Remove to favorite', props<CharacterData>())
+export const addFav = createAction('[listCharacters] Add to favorite', props<{ character:Character }>())
+export const removeFav = createAction('[listCharacters] Remove to favorite', props<{ id:number }>())
