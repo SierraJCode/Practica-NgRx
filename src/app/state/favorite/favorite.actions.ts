c4aea131cf345029faa00ac5660da0afbade31fc
Jesus Sierra <sierraja06@gmail.com>
2024-07-22 00:44:52 -0500

    Modificando action para recibir parametros

--- src/app/state/favorite/favorite.actions.ts
+++ src/app/state/favorite/favorite.actions.ts
@@ -1,4 +1,4 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
 
 export const addFav = createAction('[listCharacters] Add to favorite')
-export const removeFav = createAction('[listCharacters] Remove to favorite')
+export const removeFav = createAction('[listCharacters] Remove to favorite', props<CharacterData>())
