a7e2b03455c2bb1a1b5462378a7759e85b64518c
Jesus Sierra <sierraja06@gmail.com>
2024-07-22 01:17:46 -0500

    Reducer listo

---
+++ src/app/state/favorite/favorite.reducer.ts
@@ -0,0 +1,15 @@
+import { createReducer, on, Action } from "@ngrx/store";
+import { addFav, removeFav } from "./favorite.actions";
+import { Character } from "../../components/interfaces/character.interface";
+
+export const initialState: Character[] = [];
+
+const _favoriteReducer = createReducer(
+    initialState,
+    on(addFav, (state, {character}) => [...state, character]),
+    on(removeFav, (state, { id }) => state.filter(character => character.id != id))
+);
+
+export function favoriteReducer(state: Character[] | undefined, action: Action){
+    return _favoriteReducer(state, action);
+}
